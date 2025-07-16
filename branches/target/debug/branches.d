02155b2f0edc62ff81d803fbed2d05632edad01c
Ibtisam Mirza <iuhmirza@gmail.com>
2025-07-16 18:07:18 +0100

    Read up to chapter 3 in rust book

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/ibtisam/repos/rust-book/branches/target/debug/branches: /home/ibtisam/repos/rust-book/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-2fe8a3a7a06aef16.d
@@ -0,0 +1,5 @@
+/home/ibtisam/repos/rust-book/branches/target/debug/deps/branches-2fe8a3a7a06aef16.d: src/main.rs
+
+/home/ibtisam/repos/rust-book/branches/target/debug/deps/branches-2fe8a3a7a06aef16: src/main.rs
+
+src/main.rs:
